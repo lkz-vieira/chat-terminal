0459e7982a33502e00a841fabf62fd341254c8ab
Lucas Vieira <lucsolivier@gmail.com>
2021-03-22 19:06:43 -0300

    Controller para manipular a interação com o terminal

---
+++ client/src/terminalController.js
@@ -0,0 +1,97 @@
+import ComponentsBuilder from "./components.js";
+import { constants } from "./constants.js";
+
+export default class TerminalController {
+    #userCollors = new Map();
+
+    constructor() {}
+
+    #pickCollor() {
+        return `#${((1 << 24) * Math.random() | 0).toString(16)}-fg`
+    }
+
+    #getUserCollor(userName) {
+        if (this.#userCollors.has(userName))
+            return this.#userCollors.get(userName);
+
+        const collor = this.#pickCollor();
+        this.#userCollors.set(userName, collor);
+        return collor;
+    }
+
+    #onInputReceived(eventEmitter) {
+        return function () {
+            const message = this.getValue();
+            console.log({ message });
+            this.clearValue();
+        };
+    }
+
+    #onMessageReceveid({ screen, chat }) {
+        return (msg) => {
+            const { userName, message } = msg;
+            const collor = this.#getUserCollor(userName);
+
+            chat.addItem(`{${collor}}{bold}${userName}{/}: ${message}`);
+            screen.render();
+        };
+    }
+
+    #onLogChanged({ screen, activityLog }) {
+        return (msg) => {
+            const [userName] = msg.split(/\s/);
+            const collor = this.#getUserCollor(userName);
+            activityLog.addItem(`{${collor}}{bold}${msg.toString()}{/}`);
+            screen.render();
+        };
+    }
+
+    #onStatusChanged({ screen, status }) {
+        return (users) => {
+            const { content } = status.items.shift();
+            status.clearItems();
+            status.addItem(content);
+            users.forEach((userName) => {
+                const collor = this.#getUserCollor(userName);
+                status.addItem(`{${collor}}{bold}${userName}{/}`);
+            });
+            screen.render();
+        };
+    }
+
+    #registerEvents(eventEmitter, components) {
+        eventEmitter.on(
+            constants.events.app.MESSAGE_RECEIVED,
+            this.#onMessageReceveid(components)
+        );
+        eventEmitter.on(
+            constants.events.app.ACTIVITYLOG_UPDATED,
+            this.#onLogChanged(components)
+        );
+        eventEmitter.on(
+            constants.events.app.STATUS_UPDATED,
+            this.#onStatusChanged(components)
+        );
+    }
+
+    async initializeTable(eventEmitter) {
+        const components = new ComponentsBuilder()
+            .setScreen({ title: "Terminal Chat - Crazy Messages" })
+            .setLayoutComponent()
+            .setInputComponent(this.#onInputReceived(eventEmitter))
+            .setChatComponent()
+            .setActivityLogComponent()
+            .setStatusComponent()
+            .build();
+
+        this.#registerEvents(eventEmitter, components);
+        components.input.focus();
+        components.screen.render();
+        const users = ["lucasVieira"];
+        eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+        users.push("troll01", "noobs");
+        eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+        users.push("aninha", "hankys");
+        eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+    }
+}
