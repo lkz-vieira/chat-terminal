33cf5d078bee1070635ffc47d681d0de8d4c60d2
Lucas Vieira <lucas.vieira@br.experian.com>
2021-03-25 17:58:11 -0300

    Revert "Criado configuração para o Client se conectar via websocket"
    This reverts commit 3a5fb6378459000e25113885cd3926f5a389cd9b.

--- client/index.js
+++ client/index.js
@@ -1,14 +1,7 @@
 import Events from "events";
-import CliConfig from "./src/cliConfig.js";
-import SocketClient from "./src/socket.js";
 import TerminalController from "./src/terminalController.js";
 
-
-
-const [nodePath, filePath, ...commands ] = process.argv
-const config = CliConfig.parseArguments(commands)
-
 const componentEmitter = new Events();
-const socketClient = new SocketClient(config)
-await socketClient.initialize()
+const terminalController = new TerminalController();
 
+await terminalController.initializeTable(componentEmitter);
