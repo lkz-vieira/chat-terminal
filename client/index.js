3a5fb6378459000e25113885cd3926f5a389cd9b
Lucas Vieira <lucas.vieira@br.experian.com>
2021-03-25 17:49:02 -0300

    Criado configuração para o Client se conectar via websocket

--- client/index.js
+++ client/index.js
@@ -1,7 +1,14 @@
 import Events from "events";
+import CliConfig from "./src/cliConfig.js";
+import SocketClient from "./src/socket.js";
 import TerminalController from "./src/terminalController.js";
 
-const componentEmitter = new Events();
-const terminalController = new TerminalController();
 
-await terminalController.initializeTable(componentEmitter);
+
+const [nodePath, filePath, ...commands ] = process.argv
+const config = CliConfig.parseArguments(commands)
+
+const componentEmitter = new Events();
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
+
