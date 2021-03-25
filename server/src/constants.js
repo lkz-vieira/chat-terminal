590c5e9d9896b26c678cae4aa0bbf1c008229c99
Lucas Vieira <lucas.vieira@br.experian.com>
2021-03-25 17:57:42 -0300

    Revert "Criado servidor http para receber a conexões e processar ws"
    This reverts commit 94449aa36c83defd6b8adf1c3c7153e27ed996f9.

--- server/src/constants.js
+++
@@ -1,8 +0,0 @@
-export const constants = {
-    event: {
-        MESSAGE: 'message',
-        NEW_USER_CONNECTED: 'newUserConnected',
-        DISCONNECT_USER: 'disconnectUser',
-        UPDATE_USER: 'updateUser'
-    }
-};
