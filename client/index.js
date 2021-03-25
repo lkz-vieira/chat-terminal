0a0195b38a2d24a79141976dfb60baa5250c4b90
Lucas Vieira <lucsolivier@gmail.com>
2021-03-25 18:16:20 -0300

    Criado configuração para o Client se conectar via websocket

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 import Events from "events";
 import TerminalController from "./src/terminalController.js";
+import CliConfig from "./src/cliConfig.js";
+import SocketClient from "./src/socket.js";
+
+const [nodePath, filePath, ...commands ] = process.argv
+const config = CliConfig.parseArguments(commands)
 
 const componentEmitter = new Events();
-const terminalController = new TerminalController();
-
-await terminalController.initializeTable(componentEmitter);
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
